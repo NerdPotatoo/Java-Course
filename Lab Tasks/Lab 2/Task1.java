66b2d289ff693d046c30609246cf2d2c09d5244f
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2025-03-17 11:25:29 +0600

    Update Task1.java

--- Lab Tasks/Lab 2/Task1.java
+++ Lab Tasks/Lab 2/Task1.java
@@ -15,13 +15,13 @@ public class Task1 {
         int n = src.nextInt();
         for(int i = 1; i<=n; i++)
         {
-            // for(int space = n-i; space>0; space--)
-            // {
-            //     System.out.print(" ");
-            // }
+             for(int space = n-i; space>0; space--)
+             {
+                 System.out.print(" ");
+             }
             for(int star = 1; star<=i; star++)
             {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
